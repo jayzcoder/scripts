d33f7b6548bc04c7151cb204d65f7b18521a4512
jayzcoder <32261474+jayzcoder@users.noreply.github.com>
2024-07-20 23:22:15 +0800

    Update qb_check_ul.py
    调整为18T

--- qb_check_ul.py
+++ qb_check_ul.py
@@ -7,7 +7,7 @@ if response.status_code == 200:
     data = response.json()
     alltime_ul = data['server_state']['alltime_ul']
     print(f'Total Traffic Up: {alltime_ul}')
-    if alltime_ul > 16*1024*1024*1024*1024:
+    if alltime_ul > 18*1024*1024*1024*1024:
         subprocess.run(['systemctl', 'stop', 'qbittorrent-nox@admin.service'], check=True);
 else:
     print('Failed to retrieve data from qBittorrent')
